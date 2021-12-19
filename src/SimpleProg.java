8ee9b461898803fd90f4b9d4462f901d89e36c9e
nadeasy <kolodziej.bianca92@gmail.com>
2021-12-19 11:52:32 +0100

    init

---
+++ src/SimpleProg.java
@@ -0,0 +1,17 @@
+/**
+ * Testprog.
+ * @author Bianca Kolodziej
+ */
+public class SimpleProg {
+
+    /**
+     * Addiert zwei Zahlen.
+     * @param i1 erste Zahl
+     * @param i2 zweite Zahl
+     * @return Summe beider zahlen  
+     */
+    public static int sum(int i1, int i2) {
+        return i1 + i2;
+    }
+
+}
---
+++ test/MyTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyTest {
+
+    @Test
+    public void testSum() {
+        Assert.assertEquals(4, SimpleProg.sum(2,2));
+    }
+
+}
